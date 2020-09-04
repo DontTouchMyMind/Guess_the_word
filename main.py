fafe2ac9077e0ad5595afbbac59db8035e5a70ab
DontTouchMyMind <tobigface@gmail.com>
2020-09-04 15:20:07 +0200

    #2 Code review. Final commit.

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
 import random
 
 
-def get_used_letters_test():
-    print(f'The test of the method get_used_letters begin...')
-    test_letters = ['a', 'c', 'b', 'a']
-    game.guess_letter(test_letters[0])
-    print('#Test 1 was successful.' if game.get_used_letters() == ['a'] else '#Test 1 failed.')
-    game.guess_letter(test_letters[1])
-    print('#Test 2 was successful.' if game.get_used_letters() == ['a', 'c'] else '#Test 2 failed.')
-    game.guess_letter(test_letters[2])
-    print('#Test 3 was successful.' if game.get_used_letters() == ['a', 'b', 'c'] else '#Test 3 failed.')
-    game.guess_letter(test_letters[3])
-    print('#Test 4 was successful.' if game.get_used_letters() == ['a', 'b', 'c'] else '#Test 3 failed.')
-
-
-def get_visible_word_test():
-    print(f'The test of the method get_visible_word begin...')
-    test_letters = ['у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю']
-    game.get_random_word()
-    all_hidden_letters = game.get_visible_word()
-    for i in range(len(test_letters)):
-        game.guess_letter(test_letters[i])
-    print('#Test 1 was successful.' if game.get_visible_word() != all_hidden_letters else '#Test 1 failed.')
-
-
 class Game:
 
     def __init__(self, allowed_attempts: int):
@@ -52,9 +29,17 @@ class Game:
             self.__allowed_attempts -= 1
 
     def get_used_letters(self):
+        """
+        The method displays sorted list of used letters.
+        :return: type:list; sorted list of used letters.
+        """
         return sorted(set(self.__player_word))
 
     def get_visible_word(self):
+        """
+        The method displays current state of the game.
+        :return: type:list; list with hidden and guessed letters.
+        """
         return list(letter if letter in self.__player_word else '_' for letter in self.__game_word)
 
     @property
@@ -65,7 +50,3 @@ class Game:
 difficult_game = int(input('Enter the number of available attempts: '))
 
 game = Game(difficult_game)
-
-if __name__ == '__main__':
-    get_used_letters_test()
-    get_visible_word_test()
